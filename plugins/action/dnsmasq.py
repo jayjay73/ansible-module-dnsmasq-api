1ed498da7868febabe639558a6b9390c3c5da477
Jan Jaeger <jan@janjaeger.com>
2024-08-29 10:27:18 +0000

    clean up.

--- plugins/action/dnsmasq.py
+++ plugins/action/dnsmasq.py
@@ -1,30 +1,20 @@
 #!/usr/bin/python
 # vim:fileencoding=utf-8:tabstop=8:shiftwidth=4:showtabline:expandtab:softtabstop=4:foldmethod=marker:autoindent
 
-#from __future__ import print_function
-
-#import sys
-#import subprocess
-import requests
-from requests.auth import HTTPBasicAuth
-#from requests.exceptions import HTTPError, ConnectionError
-#import json
-#import jsonpickle
-#import os.path
-#from jsonpath_ng import jsonpath
-#from jsonpath_ng.ext import parse
-
+# {{{ imports
+#
 from ansible.plugins.action import ActionBase
 from ansible.module_utils.parsing.convert_bool import boolean
 from ansible.module_utils.common.text.converters import to_native
 from ansible.errors import AnsibleError, AnsibleAction, _AnsibleActionDone, AnsibleActionFail, AnsibleActionSkip
 from ansible.utils.display import Display
-from urllib.parse import urlparse
-#import dns.resolver
-#import copy
 
+import requests
+from requests.auth import HTTPBasicAuth
 import yaml
-#import tempfile
+from urllib.parse import urlparse
+
+# }}}
 
 # {{{ some debug stuff
 #
@@ -38,43 +28,22 @@ display = Display()
 # you get the idea...
 
 # these are mainly just helpful for debugging
-import dumper
-import pprint
+#import dumper
+#import pprint
 
 # a print function that prints to STDERR. Helpful for debugging.
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 # }}}
 
-# magic stuff. If you find out what it does exactly, let me know.
+# magic stuff. If you find out what it does, let me know.
 ALWAYS_DELEGATE_FACT_PREFIXES = frozenset((
     'discovered_interpreter_',
 ))
 
 
-# {{{ convenience functions for jsonpath
+# {{{ local functions and classes
 #
-def _fetch_list(jp_filter, data):
-    jsonpath_expr = parse(jp_filter)
-    match = jsonpath_expr.find(data)
-    return (match)
-
-def fetch_list(jp_filter, data):
-    match= _fetch_list(jp_filter, data)
-    if match:
-        result= [x.value for x in match]
-        return (result)
-    else:
-        return {}
-
-def fetch_value(jp_filter, data):
-    match= _fetch_list(jp_filter, data)
-    if match:
-        return (match[0].value)
-    else:
-        return {}
-# }}}
-
 class DNSAPIConn:
     def __init__(self, api_url):
         self.api_url= api_url
@@ -124,20 +93,6 @@ class DNSAPIConn:
         else:
             return response
 
-
-class DNSResolver:
-    def __init__(self, dns_server):
-        self.dns_servers= [dns_server]
-
-    def getIPs(self, record):
-        r = dns.resolver.Resolver()
-        r.nameservers= self.dns_servers
-        answers = r.query(record, 'A')
-        ret= []
-        for a in answers:
-            ret.append(a.to_text())
-        return ret
-
 def prune_ds(d):
     """recursively remove empty lists, empty dicts, or None elements from a dictionary"""
 
@@ -150,6 +105,18 @@ def prune_ds(d):
         return [v for v in (prune_ds(v) for v in d) if not empty(v)]
     else:
         return {k: v for k, v in ((k, prune_ds(v)) for k, v in d.items()) if not empty(v)}
+
+def reverse_records(r):
+    a_records={}
+    for ip, v in r.items():
+        for item in v:
+            if item not in a_records:
+                a_records[item]=[ip]
+            else:
+                a_records[item].append(ip)
+    return a_records
+
+# }}}
 
 
 class ActionModule(ActionBase):
@@ -172,9 +139,7 @@ class ActionModule(ActionBase):
         value= self._task.args.get('value', None)
         state= self._task.args.get('state', "present")
 
-        target_host= str(task_vars['inventory_hostname'])
-
-        # prep stuff
+        #target_host= str(task_vars['inventory_hostname'])
 
         if state != "present" and state != "absent":
             raise AnsibleError(f"Unrecognized value in argument 'state': {state}")
@@ -201,14 +166,7 @@ class ActionModule(ActionBase):
             res= api.get(zone)
             zone= str(zone)
 
-            # "turning around" dnsmasq's record
-            a_records={}
-            for ip, v in res.json().items():
-                for item in v:
-                    if item not in a_records:
-                        a_records[item]=[ip]
-                    else:
-                        a_records[item].append(ip)
+            a_records= reverse_records(res.json())
 
             if record in a_records:
                 got[zone]= {record: a_records[record]}
@@ -223,16 +181,16 @@ class ActionModule(ActionBase):
             for k,v in got[zone].items():
                 got[zone]= {k: list(sorted(set(v)))}
 
-            #wanted= copy.deepcopy(got)
             if state == "present":
                 wanted[zone]= {record: list(sorted(set(got[zone][record]) | set(wanted_ips)))}
             if state == "absent":
                 wanted[zone]= {record: list(sorted(set(got[zone][record]) - set(wanted_ips)))}
 
-	# prune empty fields, makes the diff look better
 
         if got != wanted:
             result['changed'] = True
+
+	    # prune empty fields, makes the diff look better
             got_for_diff= prune_ds(got)
             wanted_for_diff= prune_ds(wanted)
             before= None
@@ -268,20 +226,4 @@ class ActionModule(ActionBase):
                 for ip in to_remove[record]:
                     api.delete(zone, ip, record)
 
-
-        ## If you need a local temp dir
-        ##
-        #local_tempdir= None
-        #try:
-        #    local_tempdir = tempfile.mkdtemp()
-        #    
-        #    # do stuff
-
-        #except(Exception) as e:
-        #    raise AnsibleError(to_native(e))
-
-        #finally:
-        #    os.system('rm -r ' + local_tempdir)
-
-        # plugin exit point
         return result
